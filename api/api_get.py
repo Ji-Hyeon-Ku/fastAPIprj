fc7c4014cae408303618ac9ed295791303be8a10
franky.ji <franky.ji@drimaes.com>
2022-08-29 17:10:57 +0900

    file update 기능 추가
    - 단순 업데이트 기능만 추가됨.

--- api/api_get.py
+++ api/api_get.py
@@ -1,6 +1,15 @@
+import os
+
 from fastapi import APIRouter
+from starlette.responses import FileResponse
+
 from conn.db_conn import engineconn
 from conn.db_class import Test
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR,'static\\')
+IMG_DIR = os.path.join(STATIC_DIR,'images\\')
+SERVER_IMG_DIR = os.path.join('http://localhost:8000/','static/','images/')
 
 router = APIRouter()
 engine = engineconn()
@@ -10,3 +19,8 @@ session = engine.sessionmaker()
 async def first_get():
     example = session.query(Test).all()
     return example
+
+
+@router.get('/images/{file_name}')
+def get_image(file_name:str):
+    return FileResponse(''.join([IMG_DIR,file_name]))
--- api/api_post.py
+++ api/api_post.py
@@ -1,11 +1,21 @@
+import os
+import secrets
+from typing import List
+
 from fastapi import APIRouter, File, UploadFile
 from pydantic import BaseModel
 from conn.db_conn import engineconn
 from conn.db_class import Test
+from datetime import datetime
 
 router = APIRouter()
 engine = engineconn()
 session = engine.sessionmaker()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR,'static\\')
+IMG_DIR = os.path.join(STATIC_DIR,'images\\')
+SERVER_IMG_DIR = os.path.join('http://localhost:8000/','static/','images/')
 
 class Item(BaseModel):
     name: str
@@ -18,8 +28,17 @@ async def first_post(item: Item):
     session.commit()
     return item
 
-@router.post("/files/")
-async def create_file(file: bytes = File()):
-    return {"file_size": len(file)}
+@router.post("/upload-images")
+async def create_upload_file(in_files: List[UploadFile] = File(...)):
+    file_urls = []
+    for file in in_files:
+        saved_file_name = ''.join(["test", secrets.token_hex(16)])
+        print(saved_file_name)
+        file_location = os.path.join(IMG_DIR, saved_file_name)
+        with open(file_location, "wb+") as file_object:
+            file_object.write(file.file.read())
+        file_urls.append(SERVER_IMG_DIR + saved_file_name)
+    result = {'fileUrls': file_urls}
+    return result
 
 
